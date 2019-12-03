b51c0a1263f1b15ad3e9a25b95c004f7195d0053
kandarpajvalia <kandarpa9@gmail.com>
2019-12-02 22:49:20 -0500

    sending responses

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const http = require("http");
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     //process.exit();
+    res.setHeader("Contest-Type", "text/html");
+    res.write("<html>");
+    res.write("<h1>");
+    res.write("Kandarp, Hello from nodejs server");
+    res.write("</h1>");
+    res.write("</html>");
+    res.end();
 });
 
 server.listen(3000)
