14cb4b239af48525226b00f3cd30850b905122ca
kandarpajvalia <kandarpa9@gmail.com>
2019-12-03 20:03:48 -0500

    styled pages section 5

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const path = require('path');
+const rootDir = require('./util/path')
 
 const app = express();
 
@@ -10,12 +11,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(rootDir, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 });
 
 app.listen(3000);
