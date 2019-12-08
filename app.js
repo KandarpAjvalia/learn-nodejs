405b39b83c3bb7b04eeeb8ea47a689604ad2c058
kandarpajvalia <kandarpa9@gmail.com>
2019-12-07 19:43:55 -0500

    pug done

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const path = require('path');
 const rootDir = require('./util/path')
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
+//app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -20,7 +23,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
